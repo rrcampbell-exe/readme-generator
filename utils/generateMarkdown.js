f5a036255d55aef949a42b44163d8f31bdae2f01
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-07-23 10:41:53 -0500

    Update through usage section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,18 +10,62 @@ function renderLicenseLink(license) { }
 // If there is no license, return an empty string
 function renderLicenseSection(license) { }
 
+const generateTableOfContents = data => {
+  for (let i = 0; i < data.tableOfContents.length; i++) {
+    console.log(data.tableOfContents.length)
+    return `- [${data.tableOfContents[i]}](#${data.tableOfContents[i]})`;
+  }
+}
+
+const generateInstall = data => {
+  if (!data.tableOfContents.includes('Installation')) {
+    return '';
+  }
+
+  return `
+## Installation
+
+${data.installation}`
+}
+
+const generateUsage = data => {
+  if (!data.tableOfContents.includes('Usage')) {
+    return '';
+  }
+
+  return `
+## Usage
+
+${data.usage}`
+}
+
+const generateDevCredits = devCreditsArr => {
+  for (let i = 0; i < devCreditsArr.length; i++) {
+    console.log(devCreditsArr.length)
+    return `
+    ## Developed By
+    - [${data.creditsDevsName[i]}](#${data.creditsDevsProfile[i]})`;
+  }
+}
+
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
 ## Description
 ${data.description}
 
-## Table of Contents`
-// ${tableOfContents.map(({ section }) => {
-//   return `
-//   - [${section}](#${section})
-//   `
-// });
+## Table of Contents
+
+${generateTableOfContents(data)}
+
+${generateInstall(data)}
+
+${generateUsage(data)}
+
+${generateDevCredits(data)}
+`
 }
 
 module.exports = generateMarkdown;
